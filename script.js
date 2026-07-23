cfe3e27121881afeb3391727602c1f386d344aa7
shubhangi177 <shubhangii7777@gmail.com>
2026-07-23 07:26:40 +0530

    no changes

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const input = document.getElementById("todo-input")
 const addButton = document.getElementById("add-btn")
 const tasks = document.getElementById("todo-list")
 
+//try to load saved todos from localStorage(if any)
+const saved = localStorage
 addButton.addEventListener('click', function(){
     let task = input.value
     input.value = ""
