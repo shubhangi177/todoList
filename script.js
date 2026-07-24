32a86109f264632542d8c240f57be5be32f6b28f
shubhangi177 <shubhangii7777@gmail.com>
2026-07-24 13:47:30 +0530

    refresh show task

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 //select dom elements
-const input = document.getElementById("todo-input")
-const addButton = document.getElementById("add-btn")
-const tasks = document.getElementById("todo-list")
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!")
+    }else{
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value
+        listContainer.appendChild(li)
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+    inputBox.value=""
+    saveData()
+}
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    }else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove()
+        saveData()
+    }
+},false)
 
-//try to load saved todos from localStorage(if any)
-const saved = localStorage
-addButton.addEventListener('click', function(){
-    let task = input.value
-    input.value = ""
-    
-})
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask()
